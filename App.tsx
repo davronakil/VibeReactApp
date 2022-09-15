3e8a72b394f5cb754ef4f2c1ddf9334aa590b8c4
davronakil <davron@akil.me>
2022-09-15 12:55:04 -0500

    Initial commit

--- App.tsx
+++ App.tsx
@@ -4,8 +4,8 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+      <h1>Vibe</h1>
+      <p>Join, complete your unique profile, and find compatible and like-minded people around you to vibe with. </p>
+      </div>
   );
 }
