75d0d9b13f75aff1560b0be724d3ceb44784cb76
davronakil <davron@akil.me>
2022-09-17 12:44:17 -0500

    Sign In With Google Initialized

--- App.tsx
+++ App.tsx
@@ -1,11 +1,39 @@
 import * as React from 'react';
+import { useEffect } from 'react';
+
 import './style.css';
 
 export default function App() {
+
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+  }
+
+useEffect(() => {
+  // global google
+
+  google.accounts.id.initialize({
+    client_id: "277985038929-k5hctsktthdu0llb29r5m7m1mirr81km.apps.googleusercontent.com",
+    callback: handleCallbackResponse
+  })
+  google.accounts.id.renderButton(
+    document.getElementById("signInDiv"),
+    {theme: "outline", size: "large"}
+
+  )
+}, []);
+
+
+
   return (
+    
     <div>
-      <h1>Vibe</h1>
-      <p>Join, complete your unique profile, and find compatible and like-minded people around you to vibe with. </p>
-      </div>
+      <h1>Vibe MVP</h1>
+      <p>Join, complete your unique profile, and find compatible and like-minded people around you to vibe with.</p>
+    
+      <div id="signInDiv"></div>
+    
+    </div>
+      
   );
 }
