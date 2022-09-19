26fb9b087a9ee4789b43b0b96159df2904299196
davronakil <davron@akil.me>
2022-09-18 21:41:07 -0500

    some styling & css classes & sections comments added.

--- App.tsx
+++ App.tsx
@@ -1,51 +1,44 @@
 import * as React from 'react';
 import { useEffect } from 'react';
-import jwt_decode from "jwt-decode";
+import jwt_decode from 'jwt-decode';
 import './style.css';
 
 export default function App() {
-
   function handleCallbackResponse(response) {
-    console.log("Encoded JWT ID token: " + response.credential);
-    
+    console.log('Encoded JWT ID token: ' + response.credential);
+
     var userObject = jwt_decode(response.credential);
     console.log(userObject);
-    
   }
 
-useEffect(() => {
-  // global google code, latest 2022 version
-  google.accounts.id.initialize({
-    client_id: "277985038929-k5hctsktthdu0llb29r5m7m1mirr81km.apps.googleusercontent.com",
-    callback: handleCallbackResponse
-  })
-  google.accounts.id.renderButton(
-    document.getElementById("signInDiv"),
-    {theme: "outline", size: "large"}
-
-  )
-}, []);
-
-
+  useEffect(() => {
+    // global google code, latest 2022 version
+    google.accounts.id.initialize({
+      client_id:
+        '277985038929-k5hctsktthdu0llb29r5m7m1mirr81km.apps.googleusercontent.com',
+      callback: handleCallbackResponse,
+    });
+    google.accounts.id.renderButton(document.getElementById('signInDiv'), {
+      theme: 'outline',
+      size: 'large',
+    });
+  }, []);
 
   return (
-    
-    <div>
+    <div id="mainContainer" class="boxShadow">
       <h1 class="mainHeader vibeBlue center textShadow">Vibe</h1>
-      <p>Join, complete your unique profile, and find compatible and like-minded people around you to vibe with.</p>
+      <p>
+        Join, complete your unique profile, and find compatible and like-minded
+        people around you to vibe with.
+      </p>
 
       {/* if signed out */}
       <div id="signInDiv"></div>
 
-      <div id="publicFeed">
-
-      </div>    
-      
+      <div id="publicFeed"></div>
 
       {/* if signed in */}
       <div id="loggedInFeed"></div>
-    
     </div>
-      
   );
 }
